44429aa73523e5d21f226506cb4c2b863ff65e8c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-25 10:25:42 -0500

    :fire: Plugins

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,30 +1,22 @@
 return require('packer').startup(function()
     use {'wbthomason/packer.nvim'}
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
+    use {'kyazdani42/nvim-tree.lua'}
+    use {'kyazdani42/nvim-web-devicons'}
     use {"folke/todo-comments.nvim"}
     use {"nvim-lua/plenary.nvim"}
     use {'nvim-lua/popup.nvim'}
-    use {"glepnir/dashboard-nvim", opt= true}
+    use {"glepnir/dashboard-nvim"}
     use {
         'nvim-telescope/telescope.nvim',
         requires = {
             {'nvim-telescope/telescope-fzy-native.nvim'},
-            {'nvim-telescope/telescope-node-modules.nvim'},
-            {'nvim-telescope/telescope-cheat.nvim'},
-            {'nvim-telescope/telescope-packer.nvim'},
             {'nvim-telescope/telescope-media-files.nvim'}
         }
     }
     use "folke/lua-dev.nvim"
-    use {
-        'hoob3rt/lualine.nvim',
-        requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    }
+    use {'hoob3rt/lualine.nvim'}
     use {'hrsh7th/nvim-compe'}
     use 'neovim/nvim-lspconfig'
     use {'folke/tokyonight.nvim'}
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-end, {display = {open_fn = require('packer.util').float}})
+end)
